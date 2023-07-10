b4389133ecadaf203d1929be03f64c5676c07115
Andrew <dennisandrew09@gmail.com>
2023-07-10 19:21:25 -0400

    test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <>
-      <h1>Lambda Eats</h1>
+      <h1>Taco Eats</h1>
       <p>You can remove this code and create your own header</p>
     </>
   );
